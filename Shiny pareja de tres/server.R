31b41ad6ce65e824b3bb32071b41af672bacad54
ramirezgsilvia <57186618+ramirezgsilvia@users.noreply.github.com>
2019-10-31 16:56:43 +0100

    Update server.R

--- Shiny pareja de tres/server.R
+++ Shiny pareja de tres/server.R
@@ -1,22 +1,20 @@
 function(input, output) {
   
-  output$trama_principal <- renderPlot({
+  output$main_plot <- renderPlot({
     
     barplot(beaver1$time, beaver1$temp, type = "l",
-         probability = TRUE,
-         breaks = as.numeric(input$seleccion_horas),
-         xlab = "Horas",
-         main = "Temp en C",
-         col = "royalblue")
-    if (input$cuadricula){
-      abline(h=1:5,v=1:5, col= "purple")
+            probability = TRUE,
+            breaks = as.numeric(input$temp),
+            xlab = "Horas",
+            main = "Temp en C")
+    
+    
+    if (input$cuadriculas){
+      abline(h=1:5,v=1:5)
     }
     
     if (input$leyenda){
       legend(x = "topright", legend = c("No", "Yes"), fill = c("royalblue", "grey"), 
-             title = "Loan")
+           title = "Loan")
     }
-    
-    
-  })
-}
+    })
