2679207cfd626267252ca26b61dca9e711e6b314
ramirezgsilvia <57186618+ramirezgsilvia@users.noreply.github.com>
2019-10-31 16:04:19 +0100

    Update server.R

--- Shiny pareja de tres/server.R
+++ Shiny pareja de tres/server.R
@@ -2,7 +2,7 @@ function(input, output) {
   
   output$trama_principal <- renderPlot({
     
-    barplot(n_beaver1$time, n_beaver1$temp, type = "l",
+    barplot(beaver1$time, beaver1$temp, type = "l",
          probability = TRUE,
          breaks = as.numeric(input$seleccion_horas),
          xlab = "Horas",
