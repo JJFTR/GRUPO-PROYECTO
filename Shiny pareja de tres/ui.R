0de31e6e5042d08ab3108b70ff5d86e321ef17b5
ramirezgsilvia <57186618+ramirezgsilvia@users.noreply.github.com>
2019-10-31 16:57:42 +0100

    Update ui.R

--- Shiny pareja de tres/ui.R
+++ Shiny pareja de tres/ui.R
@@ -1,18 +1,19 @@
 bootstrapPage(
   
-  selectInput(inputId = "seleccion_horas",
-              label = strong = "",
+  selectInput(inputId = "temp_horas",
+              label = "temp",
               choices = c(500, 900, 1300, 1700, 2100),
               selected = 500),
   
-  plotOutput(outputId = "trama_principal", height = "300px"),
   
-  checkboxInput(inputId = "cuadricula",
+  checkboxInput(inputId = "cuadriculas",
                 label = strong("Mostras cuadricula"),
                 value = FALSE),
+  
   checkboxInput(inputId = "leyenda",
                 label = strong("Mostras leyenda"),
                 value = FALSE),
   
+  plotOutput(outputId = "main_plot", height = "300px"),
   
 )
